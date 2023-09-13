915a7f75e8868c71c158ff36f6910a1fe8f2654f
Victor John Makanda <victormakanda254@gmail.com>
2023-09-13 23:46:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+
+function appendCat(name){
+    return appendCat=[...cats,name];
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function prependCat(name){
+    return prependCat=[name,...cats];
+}
+
+function removeLastCat(){
+    return removeLastCat=cats.slice(0,cats.length - 1);
+}
+
+function removeFirstCat(name){
+    return name=cats.slice(1);
+}
+
